913ccc4ed9fe03c68d3f6558c982e4078225d7ce
xuhaihua <xuhaihua@oasgames.com>
2019-06-17 10:10:29 +0800

    BattleMapCell增加Hover脚本，用于远点范围技能的目标选择。 Next：实现远点范围技能的目标选择

---
+++ Assets/Scripts/BattleMap/TestHover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TestHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    bool IsCounting = false;
+    float hoverTime = 0;
+    void Update(){
+        if (!IsCounting)
+            return;
+        hoverTime += Time.deltaTime;
+        if (hoverTime > 2f)
+        {
+            Debug.Log("Show skill range");
+            IsCounting = false;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+        IsCounting = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        hoverTime = 0;
+        IsCounting = false;
+        Debug.Log("exit");
+    }
+}
